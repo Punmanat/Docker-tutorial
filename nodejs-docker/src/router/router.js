f5a0f81008cd29d5a75d5e4f2181705e566fefee
Punmanat <60070053@kmitl.ac.th>
2020-03-06 20:42:30 +0700

    :zap:....

--- nodejs-docker/src/router/router.js
+++ nodejs-docker/src/router/router.js
@@ -1,15 +1,8 @@
-const express = require("express")
-const router = express.Router()
-var giphy = require('giphy-api')();
+const express = require("express");
+const router = express.Router();
 
-router.get("/", (req,res)=>{
-    giphy.search('cat', function (err, data) {
-        const list_cats = data.data
-        var item = list_cats[Math.floor(Math.random() * list_cats.length)]
-        const url = item.images.downsized_large.url
-        console.log(url)
-        res.render("index", {url})
-    });
-})
+router.get("/", (req, res) => {
+    res.render("index");
+});
 
-module.exports = router
+module.exports = router;
